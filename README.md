e36244be4c4b623bb50b910b354d79195578d4ae
Paul2D <paul.maxineanu@gmail.com>
2022-08-08 19:30:52 +0300

    create weather app with vuejs and tailwindcss

---
+++ README.md
@@ -0,0 +1,16 @@
+# Weather app using vueJS and tailwindcss
+
+This app uses api from openweathermap.org
+
+Go to https://openweathermap.org/ create a free account and generate your api to use for this project
+
+## Install
+
+- npm install
+- setup .env file with api weather url and api key
+    VUE_APP_WEATHER_API_URL=https://api.openweathermap.org/data/2.5/
+    VUE_APP_WEATHER_API_KEY=your_api_key_here
+- npm run serve
+
+
+ejoy :)
---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app" :class="bgClass">
+    <div class="card">
+      <input type="text" placeholder="search city..." v-model="query" @keyup.enter="fetchWeather()">
+
+      <div v-if="error" class="error">
+        <div class="location">Invalid city name</div>
+      </div>
+      
+      <div v-else>
+        <div class="location-box">
+        <div class="location">{{ weather.city }}, {{ weather.country }}</div>
+        <div class="date">{{ date }}</div>
+      </div>
+      
+      <div class="weather-box">
+        <div class="temperature">{{ weather.temp }}℃</div>
+        <div class="weather">{{ weather.weather }}</div>
+      </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import './assets/tailwind.css';
+
+export default {
+  name: 'App',
+  components: {
+    
+  }, 
+  data () {
+    return {
+      api_key: process.env.VUE_APP_WEATHER_API_KEY,
+      api_url: process.env.VUE_APP_WEATHER_API_URL,
+      query: '',
+      weather: {},
+      date : this.getCurrentDate(), 
+      bgClass : 'bg-gray-50',
+      error : false
+    }
+  },
+  methods: {
+    fetchWeather() {
+      if(this.query != '') {
+        console.log(this.query);
+        this.$http.get(this.api_url+'weather?q='+this.query+'&units=metric&appid='+this.api_key)
+        .then(response => {
+          if(response.status == 200) {
+            this.weather = {
+              city: response.data.name,
+              country: response.data.sys.country,
+              temp: Math.round(response.data.main.temp),
+              weather: response.data.weather[0].main
+            }
+            this.changeBg();
+            this.error = false;
+          }
+        })
+        .catch(error => {
+          console.log(error.message);
+          this.error = true;
+        })
+      }
+      this.query = '';
+    },
+    changeBg() {
+      let temp = this.weather.temp;
+      if(temp <= 20) {
+        this.bgClass = 'bg-sky-200';
+      } else {
+        this.bgClass = 'bg-amber-200';
+      }
+    },
+    getCurrentDate() {
+
+      let today = new Date();
+      
+      let daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      let day = today.getDay();
+      let dayName = daysArray[day];
+
+      let monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      let month = today.getMonth();
+      let monthName = monthsArray[month];
+
+      let date = dayName +' '+ today.getDate() +' ' + monthName + ' ' + today.getFullYear();
+
+      return date;
+    },
+  },
+  mounted() {
+    this.query = 'Bucharest';
+    this.fetchWeather();
+    this.changeBg();
+    this.query = '';
+  },
+}
+</script>
+
