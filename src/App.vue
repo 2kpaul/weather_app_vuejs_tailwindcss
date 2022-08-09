c01c652bb6b9cb5fafa0d022ad87040320bc5049
Paul2D <paul.maxineanu@gmail.com>
2022-08-09 11:42:42 +0300

    add weather details

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,32 @@
       
       <div v-else>
         <div class="location-box">
-        <div class="location">{{ weather.city }}, {{ weather.country }}</div>
-        <div class="date">{{ date }}</div>
-      </div>
-      
-      <div class="weather-box">
-        <div class="temperature">{{ weather.temp }}℃</div>
-        <div class="weather">{{ weather.weather }}</div>
-      </div>
-      </div>
-      
+          <div class="location">{{ weather.city }}, {{ weather.country }}</div>
+          <div class="date">{{ date }}</div>
+        </div>
+      </div>    
+      <div class="grid grid-cols-2 gap-2">  
+        <div class="card">
+          <div class="weather-box">
+            <div class="temperature">{{ weather.temp }}</div>
+            <div class="weather">{{ weather.weather }}</div>
+            <div class="description">
+              {{ weather.weather_description }} feels like {{ weather.feels_like }}
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="weather-box-details">  
+            <div v-for="(value, name, index) in weather.details" v-bind:key="index" class="info">  
+              <span>{{ formatObjectKey(name) }}:</span>
+              <span>{{ value }}</span>
+            </div>  
+          </div>
+        </div>
+      </div> 
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -50,11 +64,22 @@ export default {
         this.$http.get(this.api_url+'weather?q='+this.query+'&units=metric&appid='+this.api_key)
         .then(response => {
           if(response.status == 200) {
+            console.log(response);
             this.weather = {
               city: response.data.name,
               country: response.data.sys.country,
-              temp: Math.round(response.data.main.temp),
-              weather: response.data.weather[0].main
+              temp: Math.round(response.data.main.temp) + '℃',
+              feels_like: Math.round(response.data.main.feels_like) + '℃',
+              weather: response.data.weather[0].main,
+              weather_description: response.data.weather[0].description,
+              details : {
+                temp_max: Math.round(response.data.main.temp_max) + '℃',
+                temp_min: Math.round(response.data.main.temp_min) + '℃',
+                wind_speed: response.data.wind.speed + 'm/s',
+                visibility: (response.data.visibility / 1000) + 'Km',
+                pressure: response.data.main.pressure + 'hPa',
+                humidity: response.data.main.humidity + '%'
+              }
             }
             this.changeBg();
             this.error = false;
@@ -91,6 +116,15 @@ export default {
 
       return date;
     },
+    formatObjectKey(key) {  
+      const formatLabel = key.replaceAll('_', ' ').split(" ");
+
+      return formatLabel.map((word) => { 
+          return word[0].toUpperCase() + word.substring(1); 
+      }).join(" ");
+
+      
+    }
   },
   mounted() {
     this.query = 'Bucharest';
