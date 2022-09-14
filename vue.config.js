735bc6d10bffabeecf600c05b6ac1a45db750603
Paul2D <paul.maxineanu@gmail.com>
2022-09-14 12:18:59 +0300

    rebuil for production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/covid_statistics_app_vuejs_tailwindcss/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/weather_app_vuejs_tailwindcss/" : "/",
 })
