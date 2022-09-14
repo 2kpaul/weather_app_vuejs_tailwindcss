b02c8b150597a42acaa88a84293dbfde62405e82
Paul2D <paul.maxineanu@gmail.com>
2022-09-14 12:13:46 +0300

    build for production

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/covid_statistics_app_vuejs_tailwindcss/" : "/",
 })
